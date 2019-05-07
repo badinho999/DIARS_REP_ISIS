1fb77bb4823a506a43cdc902494c3a87f844828a
badinho999 <badinho9@hotmail.com>
2019-05-07 01:06:23 -0500

    Creando las capas y conexión a SQL Server

---
+++ CapaAccesoDatos/Conexion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CapaAccesoDatos
+{
+    class Conexion
+    {
+        #region singleton
+        private static readonly Conexion UnicaInstancia = new Conexion();
+        public static Conexion Instancia
+        {
+            get
+            {
+                return Conexion.UnicaInstancia;
+            }
+        }
+        #endregion singleton
+
+        #region metodos
+        public SqlConnection Conectar()
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Data Source=BADINHO\\SQLEXPRESS;initial Catalog=Proyecto_DIARS_ISIS;" + "Integrated Security=true";
+            return cn;
+        }
+        #endregion metodos
+    }
+}
