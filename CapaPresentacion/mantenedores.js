46d92ae714b4928342bc0ea3a17e3ad855166dd8
badinho999 <badinho9@hotmail.com>
2019-05-14 00:52:07 -0500

    Modificando el layout

---
+++ CapaPresentacion/mantenedores.js
@@ -0,0 +1,5 @@
+﻿$('#mostrarNav').on('click', () => {
+    $('nav.side-bar').toggleClass('mostrar');
+    $('section.content,header').toggleClass('moveContent');
+    $('div.btn-mostrar').toggleClass('ajustarIcon')
+})
