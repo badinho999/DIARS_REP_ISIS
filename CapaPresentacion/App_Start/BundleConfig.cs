a75225257d99180bec71d7126d86cb0af49caaa3
Rayzor <joel.penasuarez@gmail.com>
2019-07-03 04:29:52 -0500

    css & styles finished

--- CapaPresentacion/App_Start/BundleConfig.cs
+++ CapaPresentacion/App_Start/BundleConfig.cs
@@ -22,19 +22,8 @@ namespace CapaPresentacion
                 "~/Bootstrap/styles/main_styles.css",
                 "~/Bootstrap/styles/parallax.css",
                 "~/Bootstrap/styles/responsive.css",
-                "~/Bootstrap-datetime/css/bootstrap-datepicker.css"));
-
-            bundles.Add(new StyleBundle("~/bundles/logincss")
-                .Include(
-                "~/Bootstrap/styles/bootstrap-4.1.2/bootstrap.min.css",
-                "~/Bootstrap/plugins/font-awesome-4.7.0/css/font-awesome.min.css",
-                "~/Bootstrap/plugins/OwlCarousel2-2.3.4/owl.carousel.css",
-                "~/Bootstrap/plugins/OwlCarousel2-2.3.4/owl.theme.default.css",
-                "~/Bootstrap/plugins/OwlCarousel2-2.3.4/animate.css",
-                "~/Bootstrap/plugins/jquery-datepicker/jquery-ui.css",
-                "~/Bootstrap/styles/login.css",
-                "~/Bootstrap/styles/parallax.css",
-                "~/Bootstrap/styles/responsive.css"));
+                "~/Bootstrap-datetime/css/bootstrap-datepicker.css",
+                 "~/Bootstrap/styles/multi-select.css"));
 
             bundles.Add(new StyleBundle("~/bundles/mantenedorescss")
                 .Include(
@@ -81,24 +70,6 @@ namespace CapaPresentacion
                 "~/Bootstrap/js/rooms.js",
                 "~/Bootstrap/js/google-api.js",
                 "~/Bootstrap/js/contact.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/loginjs")
-                .Include(
-                "~/Bootstrap/js/jquery-3.3.1.min.js",
-                "~/Bootstrap/styles/bootstrap-4.1.2/popper.js",
-                "~/Bootstrap/styles/bootstrap-4.1.2/bootstrap.min.js",
-                "~/Bootstrap/plugins/greensock/TweenMax.min.js",
-                "~/Bootstrap/plugins/greensock/TimelineMax.min.js",
-                "~/Bootstrap/plugins/scrollmagic/ScrollMagic.min.js",
-                "~/Bootstrap/plugins/greensock/animation.gsap.min.js",
-                "~/Bootstrap/plugins/greensock/ScrollToPlugin.min.js",
-                "~/Bootstrap/plugins/OwlCarousel2-2.3.4/owl.carousel.js",
-                "~/Bootstrap/plugins/easing/easing.js",
-                "~/Bootstrap/plugins/progressbar/progressbar.min.js",
-                "~/Bootstrap/plugins/parallax-js-master/parallax.min.js",
-                "~/Bootstrap/plugins/jquery-datepicker/jquery-ui.js",
-                "~/Bootstrap/js/custom.js",
-                "~/Bootstrap/js/login.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/mantenedoresjs")
                 .Include(
